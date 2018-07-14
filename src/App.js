3f736fc94f72187861ebd1991f744821697316fe
Carlos Martin <cjmar87@gmail.com>
2018-07-14 02:05:44 -0500

    redirect

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ class App extends Component {
   handleAuth(){
       const provider = new firebase.auth.GoogleAuthProvider();
 
-      firebase.auth().signInWithPopup(provider)
+      firebase.auth().signInWithRedirect(provider)
         .then(result => console.log(`${result.user.email} ha iniciado sesion`))
         .catch(error => console.log(`Error ${error.code}: ${error.message}`));
   }
