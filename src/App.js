1af937f0ddd010c43a83483ef6710a54dbe21939
Carlos Martin <cjmar87@gmail.com>
2018-07-14 01:18:23 -0500

    video 3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,59 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import logo from './logo.svg';
+
+import FileUpload from './FileUpload';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      user: null
+    };
+
+    this.handleAuth = this.handleAuth.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  componentWillMount(){
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ user });
+    });
+  }
+
+  handleAuth(){
+      const provider = new firebase.auth.GoogleAuthProvider();
+
+      firebase.auth().signInWithPopup(provider)
+        .then(result => console.log(`${result.user.email} ha iniciado sesion`))
+        .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+  }
+
+  handleLogout(){
+    firebase.auth().signOut()
+    .then(result => console.log(`${result.user.email} ha salido`))
+    .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+  }
+
+  renderLoginButton(){
+    if (this.state.user) {
+      return(
+        <div>
+          <img width="100" src={this.state.user.photoURL} alt={this.state.user.displayName}></img>
+          <p>Hola {this.state.user.displayName}</p>
+          <button onClick={this.handleLogout}>Salir</button>
+          <FileUpload />
+        </div>
+      );
+    }else {
+      return(
+        <button onClick={this.handleAuth}>Login with google</button>
+      );
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,7 +62,7 @@ class App extends Component {
           <h1 className="App-title">Pseudogram</h1>
         </header>
         <p className="App-intro">
-          <button onClick={this.handleAuth}>Login with google</button>
+          {this.renderLoginButton()}
         </p>
       </div>
     );
