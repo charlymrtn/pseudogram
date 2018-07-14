70b481a0c3bcde77667ce5f750bf260169a796aa
Carlos Martin <cjmar87@gmail.com>
2018-07-13 22:42:41 -0500

    auth

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Pseudogram</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <button onClick={this.handleAuth}>Login with google</button>
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyB-HT6a_Ul2aAjHR7uVxwM33AAsxdKUphs",
+    authDomain: "pseudogram-1b052.firebaseapp.com",
+    databaseURL: "https://pseudogram-1b052.firebaseio.com",
+    projectId: "pseudogram-1b052",
+    storageBucket: "pseudogram-1b052.appspot.com",
+    messagingSenderId: "28532419100"
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
