484a08e41af97e80541bd1d8398a8e2187b02f71
Carlos Martin <cjmar87@gmail.com>
2018-07-14 01:42:16 -0500

    terminamos

--- src/FileUpload.js
+++ src/FileUpload.js
@@ -1,36 +1,12 @@
 import React, { Component } from 'react';
-import firebase from 'firebase';
 
 class FileUpload extends Component{
   constructor(){
     super();
     this.state = {
-      uploadValue: 0,
-      picture: null
+      uploadValue: 0
     };
 
-    this.handleUpload = this.handleUpload.bind(this);
-  }
-
-  handleUpload(event){
-    const file = event.target.files[0];
-    const storageRef = firebase.storage().ref(`/fotos/${file.name}`);
-    const task = storageRef.put(file);
-
-    task.on('state_changed', snapshot => {
-      let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      this.setState({
-        uploadValue: percentage
-      })
-    }, error => {
-      console.log(error.message)
-    }, () => {
-      storageRef.getDownloadURL().then(url => {
-      this.setState({
-                picture: url
-        });
-      })﻿;
-    });
   }
 
   render(){
@@ -39,9 +15,7 @@ class FileUpload extends Component{
         <br/>
         <progress value={this.state.uploadValue} max="100"></progress>
         <br/>
-        <input type="file" onChange={this.handleUpload}></input>
-        <br/>
-        <img width="320" src={this.state.picture} alt=""/>
+        <input type="file" onChange={this.props.onUpload}></input>
       </div>
     )
   }
